1ef0fe5d8aa899289f46e826271416bb74bebe4c
PetrB <petrbednarski@gmail.com>
2024-03-10 15:22:19 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Udemy--vanillawebprojects-19
-Nineteenth in a series of 20 small projects in Vanilla JavaScript from the Udemy course by author Brad Traversy with Brad's original GitHub repository.
+
+**Nineteenth** in a series of **20 small projects** in **Vanilla JavaScript** from the [**Udemy course**](https://www.udemy.com/course/web-projects-with-vanilla-javascript/) by author [Brad Traversy](https://www.traversymedia.com/) with this [Brad's original GitHub repository](https://github.com/bradtraversy/vanillawebprojects).
+
+# 19: Speak Number Guessing Game
+
+Number guessing game where you speak your guess into the microphone using the speech recognition API
+
+## Project Specifications
+
+- Display UI directing user to speak guess
+- Implement speech recognition to listen to mic
+- Process user's guess and match
+- Let user know higher, lower, match or not a number
+
+## Original preview
+
+<div style="margin-bottom:30px;text-align:center;display:flex; flex-direction:column;justify-content:center;" >
+    <img src="./img/19_preview.png" alt="Original preview">
+</div>
+
+## My solution preview
+
+ <div style="text-align:center;display:flex; flex-direction:column;justify-content:center;" >
+    <img style="margin-bottom:10px" src="./img/my-solution-preview.png" alt="My solution preview">
+</div>
+
+## My solution features
+
+- same as an original
+
+# Author
+
+Website - [Petr Bednarski](https://github.com/pettik) <br>
+Frontend Mentor - [@pettik](https://www.frontendmentor.io/profile/pettik)
---
+++ script.js
@@ -0,0 +1,84 @@
+const msgEl = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+console.log('Number:', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+/// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+  const msgEdited = msg.includes('.') ? msg.slice(0, -1) : msg;
+
+  writeMessage(msgEdited);
+  checkNumber(msgEdited);
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! 🎉</h2>
+      <p> You have guessed the number!</p>
+      <h3> It was ${num} </h3>
+      <button class="play-again" id="play-again">Play Again!</button>
+   `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div> GO LOWER 👇</div>';
+  } else {
+    msgEl.innerHTML += '<div> GO HIGHER 👆</div>';
+  }
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+   <div>You said:</div>
+      <span class="box">${msg}</span>
+   `;
+}
+
+// Generate random number
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Speak result
+recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => {
+  recognition.start();
+});
+
+document.body.addEventListener('click', e => {
+  if (e.target.id === 'play-again') {
+    window.location.reload();
+  }
+
+  if (e.target.id === 'microphone') {
+    recognition.stop();
+  }
+});
